ec89bc07d381bfbc7439a396ec88f8aa9915857b
Yaoyu Hu using NUC 2019 <yaoyuh@andrew.cmu.edu>
2019-03-28 15:45:17 -0400

    Update xiAPI to 4.17.37. RAW8 now works.

--- src/StereoXiCamera.cpp
+++ src/StereoXiCamera.cpp
@@ -975,8 +975,8 @@ void StereoXiCamera::set_external_timestamp_reset(void)
 {
     // Set the external timestamp reset.
     LOOP_CAMERAS_BEGIN
-        mCams[loopIdx].SetTimeStampResetMode(XI_TS_RST_ARM_PERSIST);
-        mCams[loopIdx].SetTimeStampResetSource(XI_TS_RST_SRC_GPI_2);
+        mCams[loopIdx].SetTimestampResetMode(XI_TS_RST_ARM_PERSIST);
+        mCams[loopIdx].SetTimestampResetSource(XI_TS_RST_SRC_GPI_2);
     LOOP_CAMERAS_END
 
     // mCams[CAM_IDX_0].SetTimeStampResetMode(XI_TS_RST_ARM_PERSIST);
